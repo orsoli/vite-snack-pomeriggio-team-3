ea8617dfd8dcb11b7268fb86b220df8f97b17adc
orsoli <fljcoli@gmail.com>
2024-11-21 16:16:12 +0100

    Implemented function to get post from api andpoint in PostsGallery

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -5,9 +5,7 @@ export default {
     name: 'Home',
     data() {
         return {
-            // Variables
-            postsEndPoint: 'http://127.0.0.1:8000/api/posts',
-            posts: [], // Store initialized value for posts lists
+            
           
         };
     },
@@ -17,27 +15,8 @@ export default {
     },
 
     methods: {
-        getPostsResulsts(){
-            // Make a request for a user with a given ID
-            axios.get(this.postsEndPoint)
-                .then(function (response) {
-                    // handle success
-                    console.log(response.data.results);
-                })
-                .catch(function (error) {
-                    // handle error
-                    console.log(error);
-                })
-                .finally(function () {
-                    // always executed
-                });
-        }
-
+    
     },
-
-    mounted(){
-        this.getPostsResulsts()
-    }
 };
 </script>
 
--- src/components/pages/PostsGallery.vue
+++ src/components/pages/PostsGallery.vue
@@ -1,8 +1,13 @@
 <script>
+import axios from 'axios';
+
 export default {
+    name: 'posts.gallery',
     data() {
         return {
-          
+          // Variables
+            postsEndPoint: 'http://127.0.0.1:8000/api/posts',
+            posts: [], // Store initialized value for posts lists
         };
     },
 
@@ -11,7 +16,28 @@ export default {
     },
 
     methods: {
+        getPostsResulsts(){
+            // Make a request for a user with a given ID
+            axios.get(this.postsEndPoint)
+                .then(function (response) {
+                    // handle success
+                    console.log(response.data.results.data);
 
+                    this.posts = response.data.results.data
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                    console.log('Getting Posts successfully completed')
+                });
+        }
+    },
+
+    mounted(){
+        this.getPostsResulsts()
     }
 };
 </script>
@@ -19,4 +45,5 @@ export default {
 <template>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang='scss' scoped>
+</style>
